a87772dcdb37c5bb45683f7d22941230a4b1260b
abhilash <abhilash.kamble376@gmail.com>
2021-11-30 17:03:14 +0530

    update for button toggle

---
+++ playground.js
@@ -0,0 +1,47 @@
+
+
+
+let datas = [
+    {
+        title: "apple",
+        data: [
+            { x: 'Q1', y: 1 },
+            { x: 'Q2', y: 4 },
+            { x: 'Q3', y: 6 },
+            { x: 'Q4', y: 8 }
+        ]
+    },
+    {
+        title: "orange",
+
+        data: [
+            { x: 'Q1', y: 1 },
+            { x: 'Q2', y: 4 },
+            { x: 'Q3', y: 6 },
+            { x: 'Q4', y: 8 }
+        ]
+    },
+    {
+        title: "mango",
+
+        data: [
+            { x: 'Q1', y: 1 },
+            { x: 'Q2', y: 4 },
+            { x: 'Q3', y: 6 },
+            { x: 'Q4', y: 8 }
+        ]
+    }
+
+]
+
+
+
+
+datas = datas.filter(item => item.title !== "apple")
+console.log("🚀 ~ file: playground.js ~ line 41 ~ datas", datas)
+
+
+
+
+
+
--- src/screen/chart.js
+++ src/screen/chart.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { useState, Fragment } from 'react'
+import { useState, Fragment, useEffect } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import {
   XYPlot,
@@ -18,58 +18,87 @@ import {
 
 //   const [useCanvas, setUseCanvas] = useState(false)
 
+
+
+
 //   const [show, setShow] = useState(false);
-//   const [data, setData] = useState([]);
 //   const [name, setName] = useState("");
 
-//   const [series, setSeries] = useState([{
-//     title: "Apples",
+//   const [data, setData] = useState([{
+//     title: "apple",
 //     disabled: false,
+//     cluster: 2015,
+//     isActive: true,
+//     color: "#12939A",
 //     data: [{ x: 0, y: 12 }, { x: 1, y: 22 }, { x: 2, y: 32 }, { x: 3, y: 42 }]
 //   },
 
 //   {
-//     title: "Orange",
+//     title: "orange",
 //     disabled: false,
+//     cluster: 2015,
+//     isActive: true,
+//     color: "#79C7E3",
 //     data: [{ x: 0, y: 12 }, { x: 1, y: 22 }, { x: 2, y: 32 }, { x: 3, y: 42 }]
 //   },
 
 //   {
-//     title: "Mangos",
+//     title: "mango",
 //     disabled: false,
+//     cluster: 2015,
+//     isActive: true,
+//     color: "pink",
 //     data: [{ x: 0, y: 12 }, { x: 1, y: 22 }, { x: 2, y: 32 }, { x: 3, y: 42 }]
 //   }])
 
+//   const [items, setItems] = useState([{
+//     title: "apple",
+//     disabled: false,
+//     cluster: 2015,
+//     isActive: true,
+//     color: "#12939A",
+//     data: [{ x: 0, y: 12 }, { x: 1, y: 22 }, { x: 2, y: 32 }, { x: 3, y: 42 }]
+//   },
+
+//   {
+//     title: "orange",
+//     disabled: false,
+//     cluster: 2015,
+//     isActive: true,
+//     color: "#79C7E3",
+//     data: [{ x: 0, y: 12 }, { x: 1, y: 22 }, { x: 2, y: 32 }, { x: 3, y: 42 }]
+//   },
+
+//   {
+//     title: "mango",
+//     disabled: false,
+//     cluster: 2015,
+//     isActive: true,
+//     color: "pink",
+//     data: [{ x: 0, y: 12 }, { x: 1, y: 22 }, { x: 2, y: 32 }, { x: 3, y: 42 }]
+//   }])
+
+//   const [newData, setNewData] = useState(data)
+
+
+
 //   const BarSeries = useCanvas ? VerticalBarSeriesCanvas : VerticalBarSeries;
 
+
+
+
+
+
 //   const clickHandler = (item, i) => {
-//     const { series } = series;
-//     series[i].disabled = !series[i].disabled;
-//     setSeries({ series });
+
+
+
+
+//     items[i].disabled = !items[i].disabled;
+//     setData(data)
 //   }
 
 
-//   const appleData = [
-//     { x: 'Q1', y: 1 },
-//     { x: 'Q2', y: 4 },
-//     { x: 'Q3', y: 6 },
-//     { x: 'Q4', y: 8 }
-//   ]
-
-
-//   const orangeData = [
-//     { x: 'Q1', y: 1 },
-//     { x: 'Q2', y: 4 },
-//     { x: 'Q3', y: 6 },
-//     { x: 'Q4', y: 8 }
-//   ]
-
-//   const mangoData = [
-//     { x: 'Q1', y: 1 },
-//     { x: 'Q2', y: 4 },
-//     { x: 'Q3', y: 6 },
-//     { x: 'Q4', y: 8 }
-//   ]
 
 //   return (
 //     <div className="m-10" >
@@ -78,128 +107,41 @@ import {
 //         className="clustered-stacked-bar-chart-example"
 //         xType="ordinal"
 //         stackBy="y"
-//         width={450}
+//         width={350}
 //         height={500}
-//         onMouseLeave={() => setShow(true)}
 //       >
 //         <DiscreteColorLegend
 //           style={{ position: 'absolute', left: '500px', top: '10px' }}
 //           orientation="vertical"
-//           onItemClick={clickHandler}
-//           items={[
-//             {
-//               title: 'Apples',
-//               color: '#12939A'
-//             },
-//             {
-//               title: 'Oranges',
-//               color: '#79C7E3'
-//             },
-//             {
-//               title: 'Mangos',
-//               color: 'pink'
-//             }
-//           ]}
+//           onItemClick={(clickHandler)}
+//           items={items.map((ele) => ele)}
 
 //         />
 //         <HorizontalGridLines />
 //         <XAxis />
 //         <YAxis />
-//         <BarSeries
-//           cluster="2015"
-//           color="#12939A"
-//           data={appleData}
+//         {
+//           data.map((d) => {
+//             if (d.disabled != true) {
+//               return <BarSeries
+//                 cluster={d.cluster}
+//                 color={d.color}
+//                 data={d.data}
+//                 stroke="red"
+//               />
+//             }
 
-//           onSeriesMouseOver={() => { setShow(true); setData(appleData); setName("Apple") }}
 
+//           })
 
-//         />
-//         <BarSeries
-//           cluster="2015"
-//           color="#79C7E3"
-//           data={orangeData}
-//           onSeriesMouseOver={() => { setShow(true); setData(orangeData); setName("Orange") }}
-//         />
-//         <BarSeries
-//           cluster="2015"
-//           color="pink"
-//           data={mangoData}
-//           onSeriesMouseOver={() => { setShow(true); setData(mangoData); setName("Mango") }}
+//         }
 
-//         />
 
 //       </XYPlot>
 
 
 
-//       <Transition.Root show={show} as={Fragment}>
-//         <Dialog as="div" className="fixed inset-0 overflow-hidden" onClose={() => setShow(false)}>
-//           <div className="absolute inset-0 overflow-hidden">
-//             <Transition.Child
-//               as={Fragment}
-//               enter="ease-in-out duration-500"
-//               enterFrom="opacity-0"
-//               enterTo="opacity-100"
-//               leave="ease-in-out duration-500"
-//               leaveFrom="opacity-100"
-//               leaveTo="opacity-0"
-//             >
-//               <Dialog.Overlay className="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-//             </Transition.Child>
 
-//             <div className="fixed inset-y-0 right-0 pl-10 max-w-full flex ">
-//               <Transition.Child
-//                 as={Fragment}
-//                 enter="transform transition ease-in-out duration-500 sm:duration-700"
-//                 enterFrom="translate-x-full"
-//                 enterTo="translate-x-0"
-//                 leave="transform transition ease-in-out duration-500 sm:duration-700"
-//                 leaveFrom="translate-x-0"
-//                 leaveTo="translate-x-full"
-//               >
-//                 <div className="w-screen max-w-md">
-//                   <div className="h-full flex flex-col bg-white shadow-xl overflow-y-scroll">
-//                     <div className="flex-1 py-6 overflow-y-auto px-4 sm:px-6">
-//                       <h3 className="text-red-400">{name}</h3>
-
-//                       <div className="mt-8">
-//                         <div className="flow-root">
-//                           <div>
-
-//                             <table className="table">
-//                               <thead>
-//                                 <tr>
-//                                   <td >x</td>
-//                                   <td>y</td>
-//                                 </tr>
-//                               </thead>
-//                               <tbody>
-//                                 {data.map((d) => {
-//                                   return (
-//                                     <tr>
-//                                       <td>{d.x}</td>
-//                                       <td>{d.y}</td>
-//                                     </tr>
-
-//                                   )
-//                                 })}
-
-//                               </tbody>
-//                             </table>
-//                             <button onClick={() => setShow(false)} className="btn btn-danger">
-//                               Cancel
-//                             </button>
-//                           </div>
-//                         </div>
-//                       </div>
-//                     </div>
-//                   </div>
-//                 </div>
-//               </Transition.Child>
-//             </div>
-//           </div>
-//         </Dialog>
-//       </Transition.Root>
 //     </div >
 //   );
 
@@ -218,12 +160,14 @@ export default class Chart extends React.Component {
     this.state = {
       series: [{
         title: "Apples",
+        color: "#12939A",
         disabled: false,
         data: [{ x: 0, y: 12 }, { x: 1, y: 22 }, { x: 2, y: 32 }, { x: 3, y: 42 }]
       },
 
       {
         title: "Orange",
+        color: "#79C7E3",
         disabled: false,
         data: [{ x: 0, y: 12 }, { x: 1, y: 22 }, { x: 2, y: 32 }, { x: 3, y: 42 }]
       },
@@ -231,8 +175,13 @@ export default class Chart extends React.Component {
       {
         title: "Mangos",
         disabled: false,
+        color: "pink",
+
         data: [{ x: 0, y: 12 }, { x: 1, y: 22 }, { x: 2, y: 32 }, { x: 3, y: 42 }]
-      }],
+      }
+     
+    
+    ],
       show: false
     };
   }
@@ -282,109 +231,28 @@ export default class Chart extends React.Component {
             style={{ position: 'absolute', left: '500px', top: '10px' }}
 
           />
+          <HorizontalGridLines />
+
+          <XAxis />
+          <YAxis />
 
 
+          {
+            series.map((d) => {
+              if (d.disabled != true) {
+                return <VerticalBarSeriesCanvas
+                  cluster={d.cluster}
+                  color={d.color}
+                  data={d.data}
+                />
+              }
 
 
+            })
 
-          <VerticalBarSeriesCanvas
-            onItemClick={this.callShow}
-            data={series[0].data}
-            opacity={series[0].disabled ? 0.2 : 1}
-            strokeStyle="solid"
-            color="#12939A"
-          />
-
-          <VerticalBarSeriesCanvas
-            data={series[1].data}
-            opacity={series[1].disabled ? 0.2 : 1}
-            strokeStyle="solid"
-            color="#79C7E3"
-          />
-
-          <VerticalBarSeriesCanvas
-            data={series[2].data}
-            opacity={series[2].disabled ? 0.2 : 1}
-            strokeStyle="solid"
-            color="pink"
-          />
-
+          }
         </XYPlot>
 
-
-
-
-
-
-
-        <Transition.Root show={this.state.show} as={Fragment}>
-          <Dialog as="div" className="fixed inset-0 overflow-hidden" onClose={this.setShow}>
-            <div className="absolute inset-0 overflow-hidden">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-in-out duration-500"
-                enterFrom="opacity-0"
-                enterTo="opacity-100"
-                leave="ease-in-out duration-500"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
-              >
-                <Dialog.Overlay className="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-              </Transition.Child>
-
-              <div className="fixed inset-y-0 right-0 pl-10 max-w-full flex ">
-                <Transition.Child
-                  as={Fragment}
-                  enter="transform transition ease-in-out duration-500 sm:duration-700"
-                  enterFrom="translate-x-full"
-                  enterTo="translate-x-0"
-                  leave="transform transition ease-in-out duration-500 sm:duration-700"
-                  leaveFrom="translate-x-0"
-                  leaveTo="translate-x-full"
-                >
-                  <div className="w-screen max-w-md">
-                    <div className="h-full flex flex-col bg-white shadow-xl overflow-y-scroll">
-                      <div className="flex-1 py-6 overflow-y-auto px-4 sm:px-6">
-                        {/* <h3 className="text-red-400">{name}</h3> */}
-
-                        <div className="mt-8">
-                          <div className="flow-root">
-                            <div>
-
-                              {/* <table className="table">
-                              <thead>
-                                <tr>
-                                  <td >x</td>
-                                  <td>y</td>
-                                </tr>
-                              </thead>
-                              <tbody>
-                                {data.map((d) => {
-                                  return (
-                                    <tr>
-                                      <td>{d.x}</td>
-                                      <td>{d.y}</td>
-                                    </tr>
-
-                                  )
-                                })}
-
-                              </tbody>
-                            </table> */}
-                              <button onClick={this.setShow} className="btn btn-danger">
-                                Cancel
-                              </button>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </Transition.Child>
-              </div>
-            </div>
-          </Dialog>
-        </Transition.Root>
       </div >
     );
   }
